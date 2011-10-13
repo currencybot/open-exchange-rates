14cc88b56b2c415f73458e2679abd06d4c5bea2c
Joss Crowcroft <josscrowcroft@gmail.com>
2011-10-13 05:46:08 +0100

    Fix for scraper issue, need to recreate everything on each crawl

--- code/scraper.js
+++ code/scraper.js
@@ -1,5 +1,5 @@
 /**
- * scraper.js - part of the open-source exchange rates project
+ * scraper.js v0.1 - part of the open-source exchange rates project
  * by @josscrowcroft - josscrowcroft.com
  *
  * Scrapes currency conversion rates one-by-one from Google Calculator, saves them
@@ -10,7 +10,7 @@
  * `latest.json` contains the most recent exchange rates
  * `historical/[YYYY-MM-DD].json` contains exchange rates from past days (eg. 2011-10-09.json)
  *
- * More info: http://josscrowcroft.github.com/open-source-exchange-rates/
+ * More info: http://josscrowcroft.github.com/open-exchange-rates/
  *
  * License: http://www.opensource.org/licenses/GPL-3.0
  */
@@ -126,14 +126,9 @@ var baseCurrency = "USD",
 		"ZAR",
 		"ZMK"
 	],
-	requests = _.map(currencies, function(each) {
-		return {
-			method: 'GET',
-			cc : each,
-			uri: 'ig/calculator?hl=en&q=1' + baseCurrency + '=?' + each
-		}
-	}),
-	responses = {};
+	agent,
+	requests,
+	responses;
 
 
 // Log function, only logs if `--log` specified in argv:
@@ -149,82 +144,93 @@ function getUTC() {
 	return Date.parse('now').add({ hours: -parseInt(Date.today().getUTCOffset(), 10) });
 }
 
-// Starts the agent with a log message:
+
+// Sets up the agent and starts it with a log message:
 function startAgent() {
+	// Create the array of requests:
+	requests = _.map(currencies, function(each) {
+		return {
+			method: 'GET',
+			cc : each,
+			uri: 'ig/calculator?hl=en&q=1' + baseCurrency + '=?' + each
+		}
+	});
 	responses = {};
+
+	// Create the HTTP agent:
+	agent = httpAgent.create('www.google.com', requests);
+
+
+	// The scraper:
+	agent.addListener('next', function (e, agent) {
+		var data;
+	log(agent.current.cc);
+		// Try to parse JSON with cleaned up calculator response:
+		try {
+			data = JSON.parse(agent.body.replace('lhs', '"lhs"').replace('rhs', '"rhs"').replace('error', '"error"').replace('icc', '"icc"'));
+		}
+		catch(e) {}
+	
+		// Add this currency conversion rate to the responses object if valid:
+		if ( data ) {
+			responses[agent.current.cc] = parseFloat(data.rhs.replace(/[^0-9-.]/g, ''));
+		}
+	
+		// Move on to the next request after `throttle` milliseconds (spreads the requests to avoid bans):
+		setTimeout(function() {
+			agent.next();
+		}, argv.throttle );
+	});
+	
+	
+	// Save the file and push to git when agent is done collecting data:
+	agent.addListener('stop', function (e, agent) {
+	
+		// Build API data:
+		var api = {
+			disclaimer : "This data is collected from Google Calculator and provided free of charge for informational purposes only, with no guarantee whatsoever of accuracy, validity, availability or fitness for any purpose; use at your own risk. Other than that - have fun, and please share/watch/fork if you think data like this should be free!",
+			license : "http://www.opensource.org/licenses/GPL-3.0",
+			timestamp : Math.round( ( getUTC() ).getTime() / 1000 ),
+			base: baseCurrency,
+			rates : responses
+		};
+	
+		log("[" + new Date().toUTCString() + "]: agent finished");
+		
+		// Write the latest and historical files, then commit and push to git when all done:
+		async.parallel(
+			[
+				function(callback) {
+					fs.writeFile("latest.json", JSON.stringify(api, false, "\t"), function(err) {
+						callback(err);
+					});
+		 		},
+				function(callback) {
+					var filename = "./historical/" + (getUTC()).toString("yyyy-MM-dd") + ".json";
+					fs.writeFile(filename, JSON.stringify(api, false, "\t"), function(err) {
+						callback(err);
+					});
+		 		}
+			],
+			function(err) {
+				// To do: add some error/success logging:
+				if( !err && !argv.nocommit ) {
+					// Commit changes to git repository and push when done:
+					exec('git add . && git commit -am "exchange rates as of [' + new Date().toUTCString() + ']"', function(err, stdout, stderr) {
+						if ( !argv.nopush ) {
+							exec('git push origin master');
+						}
+					});
+				}
+			}
+		);
+	
+	});
+
+	// Start the agent and log it:
 	agent.start();
 	log("[" + new Date().toUTCString() + "]: agent started");
 }
-
-
-// Create the HTTP agent:
-var agent = httpAgent.create('www.google.com', requests);
-
-
-// The scraper:
-agent.addListener('next', function (e, agent) {
-	var data;
-
-	// Try to parse JSON with cleaned up calculator response:
-	try {
-		data = JSON.parse(agent.body.replace('lhs', '"lhs"').replace('rhs', '"rhs"').replace('error', '"error"').replace('icc', '"icc"'));
-	}
-	catch(e) {}
-
-	// Add this currency conversion rate to the responses object if valid:
-	if ( data ) {
-		responses[agent.current.cc] = parseFloat(data.rhs.replace(/[^0-9-.]/g, ''));
-	}
-
-	// Move on to the next request after `throttle` milliseconds (spreads the requests to avoid bans):
-	setTimeout(function() {
-		agent.next();
-	}, argv.throttle );
-});
-
-
-// Save the file and push to git when agent is done collecting data:
-agent.addListener('stop', function (e, agent) {
-
-	// Build API data:
-	var api = {
-		disclaimer : "This data is collected from Google Calculator and provided free of charge for informational purposes only, with no guarantee whatsoever of accuracy, validity, availability or fitness for any purpose; use at your own risk. Other than that - have fun, and please share/watch/fork if you think data like this should be free!",
-		license : "http://www.opensource.org/licenses/GPL-3.0",
-		timestamp : Math.round( ( getUTC() ).getTime() / 1000 ),
-		base: baseCurrency,
-		rates : responses
-	};
-
-	// Write the latest and historical files, then commit and push to git when all done:
-	async.parallel(
-		[
-			function(callback) {
-				fs.writeFile("latest.json", JSON.stringify(api, false, "\t"), function(err) {
-					callback(err);
-				});
-	 		},
-			function(callback) {
-				var filename = "./historical/" + (getUTC()).toString("yyyy-MM-dd") + ".json";
-				fs.writeFile(filename, JSON.stringify(api, false, "\t"), function(err) {
-					callback(err);
-				});
-	 		}
-		],
-		function(err) {
-			// To do: add some error/success logging:
-			if( !err && !argv.nocommit ) {
-				// Commit changes to git repository and push when done:
-				exec('git add . && git commit -am "exchange rates as of [' + new Date().toUTCString() + ']"', function(err, stdout, stderr) {
-					if ( !argv.nopush ) {
-						exec('git push origin master');
-					}
-				});
-			}
-		}
-	);
-
-	log("[" + new Date().toUTCString() + "]: agent finished");
-});
 
 
 // Do the dirty business:
@@ -236,4 +242,5 @@ setInterval(function() {
 }, argv.sleep);
 
 
-// That's all, folks!
+// Take it away, currency bot!
+// [http://currencybot.github.com]
